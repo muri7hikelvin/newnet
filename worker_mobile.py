fbd96dff2955808b8a7774c50f7a5cea938b0536
muri7hi Labs <muri7hikelvin@gmail.com>
2025-09-08 02:40:02 +0300

    Update worker_mobile.py

--- worker_mobile.py
+++ worker_mobile.py
@@ -6,7 +6,7 @@ import psutil
 import os
 
 DEVICE_ID = str(uuid.uuid4())[:8]
-COORDINATOR_URI = "ws://192.168.100.2:5000"
+COORDINATOR_URI = "ws://192.168.100.5:5000"
 
 def get_cpu_free():
     try:
